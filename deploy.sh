75e3ede0f511cf836bc36b1435d35532120977c5
Mai Việt <maiviet@MacBook-Pro-cua-Mai.local>
2026-04-02 15:48:54 +0700

    add .

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+# Đảm bảo dùng Java 21
+export JAVA_HOME=/usr/lib/jvm/java-21-openjdk-amd64
+export PATH=$JAVA_HOME/bin:$PATH
+
 echo ">>> Bước 1: Build JAR (Maven local)..."
 cd backend
 mvn package -DskipTests -q
